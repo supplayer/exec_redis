e4b684d2273f260dbbe881567279c7fa611fba45
supplayer <x254724521@hotmail.com>
2021-04-13 00:04:17 +0800

    optimize ExecRedisClients

--- execredis/connection.py
+++ execredis/connection.py
@@ -1,20 +1,9 @@
 from redis import ConnectionPool, StrictRedis
 
 
-class ExecRedisClients:
-    def __init__(self, configs: dict, *args):
-        """
-        configs = dict('redis_clients_name_1': ip1, 'redis_clients_name_2': ip2...)
-        args = dict('name'=redis_clients_name, 'db'=redis_db_num, **kwargs)
-        """
-        self.redis_config = args
-        self.config = configs
-
-    def __getitem__(self, client_name) -> StrictRedis:
-        return self.clients[client_name]
-
-    @property
-    def clients(self):
-        return {i['name']: StrictRedis(
-            connection_pool=ConnectionPool(host=self.config[i.pop('name')], db=i.pop('db', 0)),
-            decode_responses=True, **i) for i in self.redis_config}
+class ExecRedisClients(StrictRedis):
+    def __init__(self, host, db, **kwargs):
+        super(ExecRedisClients, self).__init__({**dict(
+            connection_pool=ConnectionPool(host=host, db=db),
+            decode_responses=True), **kwargs}
+        )
