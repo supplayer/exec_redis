a1d95c729f2ad33114901f40f6cc0006d56a2ad8
supplayer <x254724521@hotmail.com>
2021-07-23 11:30:36 +0800

    change handler class name to ExecRedisClients

--- execredis/handler.py
+++ execredis/handler.py
@@ -1,7 +1,7 @@
 from execredis.connection import ClientsConnection, ClusterClientsConnection
 
 
-class HandlerRedis:
+class ExecRedisClients:
     def __new__(cls, clients_name, redis_config=None):
         cls.__clients_name = clients_name
         cls.__redis_config = (redis_config or {clients_name: {}})[cls.__clients_name]
