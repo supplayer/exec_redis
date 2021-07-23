35fc5d3010576c286386dd75bd1f5ddf7097ff03
supplayer <x254724521@hotmail.com>
2021-07-23 11:22:06 +0800

    fix got an unexpected keyword argument 'cluster' and init import error

--- execredis/handler.py
+++ execredis/handler.py
@@ -5,6 +5,6 @@ class HandlerRedis:
     def __new__(cls, clients_name, redis_config=None):
         cls.__clients_name = clients_name
         cls.__redis_config = (redis_config or {clients_name: {}})[cls.__clients_name]
-        if cls.__redis_config.pop('cluster'):
+        if cls.__redis_config.pop('cluster', None):
             return ClusterClientsConnection(**cls.__redis_config)
         return ClientsConnection(**cls.__redis_config)
