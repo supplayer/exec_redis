d5905242e39ddb47da5ef4ee3e491b418b76a8e4
supplayer <x254724521@hotmail.com>
2021-08-05 05:22:31 +0800

    ExecRedisClients add func create

--- execredis/handler.py
+++ execredis/handler.py
@@ -4,3 +4,8 @@ from execredis.connection import ClientsConnection, ClusterClientsConnection
 class ExecRedisClients:
     def __new__(cls, config, cluster=False):
         return ClusterClientsConnection(**config) if cluster else ClientsConnection(**config)
+
+    @classmethod
+    def create(cls, client: str, clients_config: dict):
+        _config = dict(clients_config[client])
+        return cls(_config, cluster=_config.pop("cluster", False))
