b5968094123b755eefee714fb170feda40f88652
supplayer <x254724521@hotmail.com>
2022-01-30 08:09:36 +0800

    optimize execredis.handler.ExecMappingData

--- execredis/handler.py
+++ execredis/handler.py
@@ -11,19 +11,15 @@ class ExecMappingData:
         return loads(self.__redis_app.hget(
             f"{self.__proj_name}." + 'MappingData', key=name.lower()) or b'null') or default_value
 
-    def setup_mapping_data(self, MappingData: type, mapping_data_default, value=None):
+    def redis_setup_mapping_data(self, MappingData: type, mapping_data_default):
         default_value = mapping_data_default
         mapping_data = MappingData.__dict__
         name = list(mapping_data.keys())[list(mapping_data.values()).index(default_value)]
-        if not value:
-            mapping_value = default_value
-        else:
-            if not isinstance(value, type(default_value)):
-                raise ValueError(f'Value: {value} type not match default.')
-            mapping_value = value
-        self.__setup_mapping_data(name, mapping_value)
+        self.__hdel_mapping_data(name)
+        default_value = MappingData.__dict__[name]
+        self.__setup_mapping_data(name, default_value)
 
-    def setup_all_mapping_data(self, MappingData: type, support_types=(list, int, dict, str)):
+    def redis_setup_all_mapping_data(self, MappingData: type, support_types=(list, int, dict, str)):
         flag = input('Will set all MappingData with default value on Redis. Y/N:')
         if flag == 'Y':
             for k, v in MappingData.__dict__.items():
@@ -36,3 +32,6 @@ class ExecMappingData:
 
     def __hset_mapping_data(self, key=None, value=None, mapping=None):
         return self.__redis_app.hset(f"{self.__proj_name}." + 'MappingData', key=key, value=value, mapping=mapping)
+
+    def __hdel_mapping_data(self, *keys):
+        return self.__redis_app.hdel(f"{self.__proj_name}." + 'MappingData', *keys)
